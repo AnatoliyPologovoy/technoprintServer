59199e82a0c72b7adc7c0b41ba0c4520ebfe3451
Anatoliy Pologovoy <pologovoy89@mail.ru>
2023-11-30 11:21:35 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const knifes_1 = require("./knifes");
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+// For testing purposes
+app.get('/', (req, res) => {
+    res.send("<h2>It's Working  1 23!</h2>");
+});
+app.get('/knifes', (req, res) => {
+    res.send(knifes_1.knifes);
+});
+app.get('/knifes/:number', (req, res) => {
+    const knifesNumber = req.params.number;
+    const knifesResult = knifes_1.knifes.find(item => item.number === +knifesNumber);
+    if (knifesResult) {
+        res.send(knifesResult);
+    }
+    else
+        res.status(403).send('Knife not found');
+});
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, {Request, Response} from 'express'
+import { knifes } from './knifes';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// For testing purposes
+app.get('/', (req: Request, res: Response) => {
+    res.send("<h2>It's Working  1 23!</h2>");
+});
+app.get('/knifes',(req: Request, res: Response) => {
+    res.send(knifes); 
+})
+
+app.get('/knifes/:number',(req: Request, res: Response) => {
+    const knifesNumber = req.params.number
+    const knifesResult = knifes.find(item => item.number === +knifesNumber)
+    if (knifesResult) {
+        res.send(knifesResult); 
+    }
+    else res.status(403).send('Knife not found')
+})
+
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+});
